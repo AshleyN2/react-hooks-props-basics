bd7f3b0f739710ec2a5a0dfbe5fcb173d4647080
Ashley <ashleynjeri7@gmail.com>
2022-09-24 20:22:21 +0300

    Code along commit

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function BlogContent(props) {
+  // tells React to place value of props.articleText in div 
   return <div id="blog-content">{props.articleText}</div>;
 }
 
